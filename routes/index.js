4df6e5634f60fee78bce855ff2fa9b36e11d51e9
BeDennis123 <dennisuperb@gmail.com>
2023-10-20 03:29:05 +0800

    first commit

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require("express");
 var router = express.Router();
 
 const { connectToDB, ObjectId } = require("../utils/db");
+const { route } = require("../app");
+
 
 
 /* GET event form */
@@ -14,15 +16,14 @@ router.get("/event/new", async function (req, res) {
 });
 
 /* Handle the Form */
-router.post("/event/new/submit", async function (req, res) {
+router.post("/event/new", async function (req, res) {
   const db = await connectToDB();
   try {
     console.log(req.body);
-    // req.body.createdAt = new Date();
-    // req.body.modifiedAt = new Date();
-
-    // let result = await db.collection("bookings").insertOne(req.body);
-    res.status(201).json({ msg: success });
+    let result = await db.collection("events").insertOne(req.body);
+    res
+      .status(201)
+      .render("pages/event/new", { message: "Record Created Success, copy this link to the home page: localhost:3000/" });
   } catch (err) {
     res.status(400).json({ message: err.message });
   } finally {
@@ -30,67 +31,17 @@ router.post("/event/new/submit", async function (req, res) {
   }
 });
 
-
-/* Display all Bookings */
-router.get("/event", async function (req, res) {
-  const db = await connectToDB();
-  try {
-    let results = await db.collection("bookings").find().toArray();
-    res.render("pages/event", { bookings: results });
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  } finally {
-    await db.client.close();
-  }
-});
-
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Expresso" });
-});
-
-/* Handle the Form */
-router.post("/bookings", async function (req, res) {
-  const db = await connectToDB();
-  try {
-    req.body.numTickets = parseInt(req.body.numTickets);
-    req.body.terms = req.body.terms == "on";
-    req.body.createdAt = new Date();
-    req.body.modifiedAt = new Date();
-
-    let result = await db.collection("bookings").insertOne(req.body);
-    res.status(201).json({ id: result.insertedId });
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  } finally {
-    await db.client.close();
-  }
-});
-
-/* Display all Bookings */
-router.get("/bookings", async function (req, res) {
-  const db = await connectToDB();
-  try {
-    let results = await db.collection("bookings").find().toArray();
-    res.render("bookings", { bookings: results });
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  } finally {
-    await db.client.close();
-  }
-});
-
-/* Display a single Booking */
-router.get("/bookings/read/:id", async function (req, res) {
+/* GET current event form */
+router.get("/event/edit/:id", async function (req, res) {
   const db = await connectToDB();
   try {
     let result = await db
-      .collection("bookings")
+      .collection("events")
       .findOne({ _id: new ObjectId(req.params.id) });
     if (result) {
-      res.render("booking", { booking: result });
+      res.render("pages/event/edit", { event: result });
     } else {
-      res.status(404).json({ message: "Booking not found" });
+      res.status(404).json({ message: "Event not found, copy this link to the home page: localhost:3000/" });
     }
   } catch (err) {
     res.status(400).json({ message: err.message });
@@ -99,61 +50,27 @@ router.get("/bookings/read/:id", async function (req, res) {
   }
 });
 
-// Delete a single Booking
-router.post("/bookings/delete/:id", async function (req, res) {
+// Update a single event
+router.post("/event/edit/:id", async function (req, res) {
   const db = await connectToDB();
   try {
-    let result = await db
-      .collection("bookings")
-      .deleteOne({ _id: new ObjectId(req.params.id) });
-    if (result.deletedCount > 0) {
-      res.status(200).json({ message: "Booking deleted" });
-    } else {
-      res.status(404).json({ message: "Booking not found" });
-    }
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  } finally {
-    await db.client.close();
-  }
-});
-
-// display the update form
-router.get("/bookings/update/:id", async function (req, res) {
-  const db = await connectToDB();
-  try {
-    let result = await db
-      .collection("bookings")
-      .findOne({ _id: new ObjectId(req.params.id) });
-    if (result) {
-      res.render("update", { booking: result });
-    } else {
-      res.status(404).json({ message: "Booking not found" });
-    }
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  } finally {
-    await db.client.close();
-  }
-});
-
-// Update a single Booking
-router.post("/bookings/update/:id", async function (req, res) {
-  const db = await connectToDB();
-  try {
-    req.body.numTickets = parseInt(req.body.numTickets);
-    req.body.terms = req.body.terms == "on";
-    req.body.superhero = req.body.superhero || "";
-    req.body.modifiedAt = new Date();
+    req.body.eventTitle = req.body.eventTitle || "";
+    req.body.organizer = req.body.organizer || "";
+    req.body.datetime = req.body.datetime;
+    req.body.location = req.body.location || "";
+    req.body.description = req.body.description || "";
+    req.body.quota = parseInt(req.body.quota);
+    req.body.image = req.body.image || "";
+    req.body.highlight = req.body.highlight || "";
 
     let result = await db
-      .collection("bookings")
+      .collection("events")
       .updateOne({ _id: new ObjectId(req.params.id) }, { $set: req.body });
 
     if (result.modifiedCount > 0) {
-      res.status(200).json({ message: "Booking updated" });
+      res.status(200).json({ message: "Event updated, copy this link to the home page: localhost:3000/" });
     } else {
-      res.status(404).json({ message: "Booking not found" });
+      res.status(404).json({ message: "Event not found, copy this link to the home page: localhost:3000/" });
     }
   } catch (err) {
     res.status(400).json({ message: err.message });
@@ -162,46 +79,41 @@ router.post("/bookings/update/:id", async function (req, res) {
   }
 });
 
-// Search Bookings
-router.get("/bookings/search", async function (req, res) {
-  const db = await connectToDB();
+
+/* GET home page. */
+//=search function
+router.get("/", async function (req, res, next) {
   try {
-    let query = {};
-    if (req.query.email) {
-      // query.email = req.query.email;
-      query.email = { $regex: req.query.email };
-    }
-    if (req.query.numTickets) {
-      query.numTickets = parseInt(req.query.numTickets);
-    }
-
-    let result = await db.collection("bookings").find(query).toArray();
-    res.render("bookings", { bookings: result });
+    const db = await connectToDB();
+    let result = await db.collection("events").find({}, {}).toArray();
+    let slides = await db.collection("events").find({ terms: true }, {}).toArray();
+    res.render("pages/event/index", { events: result, slides: slides});
   } catch (err) {
     res.status(400).json({ message: err.message });
-  } finally {
-    await db.client.close();
   }
 });
+
+
 
 // Pagination based on query parameters page and limit, also returns total number of documents
-router.get("/bookings/paginate", async function (req, res) {
+router.get("/event", async function (req, res) {
   const db = await connectToDB();
   try {
     let page = parseInt(req.query.page) || 1;
-    let perPage = parseInt(req.query.perPage) || 10;
+    let perPage = parseInt(req.query.perPage) || 6;
     let skip = (page - 1) * perPage;
 
     let result = await db
-      .collection("bookings")
+      .collection("events")
       .find()
       .skip(skip)
       .limit(perPage)
       .toArray();
-    let total = await db.collection("bookings").countDocuments();
+    let total = await db.collection("events").countDocuments();
+    console.log(result);
 
-    res.render("paginate", {
-      bookings: result,
+    res.render("pages/event/event", {
+      events: result,
       total: total,
       page: page,
       perPage: perPage,
@@ -212,5 +124,112 @@ router.get("/bookings/paginate", async function (req, res) {
     await db.client.close();
   }
 });
+
+/* Handle the Form */
+router.post("/event", async function (req, res) {
+  const db = await connectToDB();
+  try {
+    req.body.numTickets = parseInt(req.body.numTickets);
+    req.body.terms = req.body.terms == "on";
+    req.body.createdAt = new Date();
+    req.body.modifiedAt = new Date();
+
+    let result = await db.collection("events").insertOne(req.body);
+    res.status(201).json({ id: result.insertedId });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  } finally {
+    await db.client.close();
+  }
+});
+
+
+/* Display a single event */
+router.get("/event/detail/:id", async function (req, res) {
+  const db = await connectToDB();
+  try {
+    let result = await db
+      .collection("events")
+      .findOne({ _id: new ObjectId(req.params.id) });
+    if (result) {
+      res.render("pages/event/detail", { event: result });
+    } else {
+      res.status(404).json({ message: "event not found, copy this link to the home page: localhost:3000/" });
+    }
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  } finally {
+    await db.client.close();
+  }
+});
+
+// Delete a single event
+router.post("/event/delete/:id", async function (req, res) {
+  const db = await connectToDB();
+  try {
+    let result = await db
+      .collection("events")
+      .deleteOne({ _id: new ObjectId(req.params.id) });
+    if (result.deletedCount > 0) {
+      res.status(200).json({ message: "event deleted, copy this link to the home page: localhost:3000/" });
+    } else {
+      res.status(404).json({ message: "event not found, copy this link to the home page: localhost:3000/" });
+    }
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  } finally {
+    await db.client.close();
+  }
+});
+
+
+// Search events
+//change it for 
+// router.get("/event/search", async function (req, res) {
+//   const db = await connectToDB();
+//   try {
+//     let query = {};
+//     if (req.query.email) {
+//       // query.email = req.query.email;
+//       query.email = { $regex: req.query.email };
+//     }
+//     if (req.query.numTickets) {
+//       query.numTickets = parseInt(req.query.numTickets);
+//     }
+
+//     let result = await db.collection("events").find(query).toArray();
+//     res.render("pages/event/event", { events: result });
+//   } catch (err) {
+//     res.status(400).json({ message: err.message });
+//   } finally {
+//     await db.client.close();
+//   }
+// });
+
+/* GET volunteer form */
+router.get("/event/become", async function (req, res) {
+  try {
+    res.render("pages/event/become");
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+/* Handle the Form */
+router.post("/event/become", async function (req, res) {
+  const db = await connectToDB();
+  try {
+    console.log(req.body);
+    let result = await db.collection("volunteers").insertOne(req.body);
+    res
+      .status(201)
+      .render("pages/event/become", { message: "Record Created Success, copy this link to the home page: localhost:3000/" });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  } finally {
+    await db.client.close();
+  }
+});
+
 
 module.exports = router;
--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,7 @@
 const { MongoClient, ObjectId } = require('mongodb');
 
 // TODO: Updte your own MongoDB connection string
-process.env.MONGODB_URI = 'mongodb://admin:43JEjQfx@192.168.0.123:27017/?readPreference=primary&ssl=false&directConnection=true&authMechanism=DEFAULT'
+process.env.MONGODB_URI = 'mongodb://dennis:pQc3yxAdDQH97dNh7TFVik50PYnCKZ6z8AQos10qhmabjCkPf9lZuAPWyAeKexUCWeZo3aRvUWrpACDbXLDZeg==@dennis.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@dennis@.documnets.azure.com:10255/?ssl=true';
 if (!process.env.MONGODB_URI) {
     // throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
     process.env.MONGODB_URI = 'mongodb://localhost:27017';
@@ -9,8 +9,8 @@ if (!process.env.MONGODB_URI) {
 
 // Connect to MongoDB
 async function connectToDB() {
-    const client = await MongoClient.connect(process.env.MONGODB_URI, { useUnifiedTopology: true });
-    const db = client.db('bookingsDB');
+    const client = await MongoClient.connect(process.env.MONGODB_URI);
+    const db = client.db('eventsDB');
     db.client = client;
     return db;
 }
